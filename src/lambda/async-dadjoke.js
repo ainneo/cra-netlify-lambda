e5964e1d6ecb456c6337849b83b28c1c4eb903bb
ainneo <38201574+ainneo@users.noreply.github.com>
2021-03-01 22:20:29 -0800

    Update async-dadjoke.js

--- src/lambda/async-dadjoke.js
+++ src/lambda/async-dadjoke.js
@@ -20,3 +20,22 @@ export async function handler(event, context) {
     }
   }
 }
+
+
+
+// export async function handler(event, context) {
+//   try {
+//     const response = await axios.get("https://gnews.io/api/v4/search?q=technology&token=e75d97437a447619e7baf217e5c2165d", { headers: { Accept: "application/json" })
+//     const data = response.data
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ data })
+//     }
+//   } catch (err) {
+//     console.log(err) // output to netlify function log
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ msg: err.message }) // Could be a custom message or object i.e. JSON.stringify(err)
+//     }
+//   }
+// }
